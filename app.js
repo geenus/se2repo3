b238c97eeb7feab0d0b41974b2857693852a54a2
g1nus <giuliano.andronic@studenti.unitn.it>
2018-11-02 16:38:33 +0100

    update app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const sum = require('./sum');
 
 app.get('/', function(req, res){
-		res.send("sum of 22 + 22 is : " + sum(22,22));
+		res.send("sum of 22 + 23 is : " + sum(22,23));
 });
 
 app.listen(PORT, function(){
